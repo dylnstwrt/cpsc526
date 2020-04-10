edf156404c2b8e9ca08d0f16783d398d710cce77
dylnstwrt <dylnstwrt@gmail.com>
2020-04-10 11:30:16 -0600

    final log_analysis revision - requires edit for submission

--- A4/log_analysis.py
+++ A4/log_analysis.py
@@ -1,48 +1,58 @@
 import json
 import sys
 import ipinfo
+from tqdm import tqdm
 
 def main():
     
-    """ 
+    
     access_token = '6736a407f8b7a6'
     handler = ipinfo.getHandler(access_token)
-     """
+    
      
     countFailed = 0
     countSuccessful = 0
+    
     root_1st_try = 0
     max_root_attempts = 0;
     
-    usernames_succlogin = dict();
+    usernames_failedlogin = dict()
+    usernames_succlogin = dict()
     
-    usernames_failedlogin = dict();
-    ip_failedlogin = dict();
-    
-    passwords_attempted = dict();
-    ip_firsttime = set();
-    
-    commands_entered = dict();
+    ip_failedlogin = dict()
+    ip_firsttime = set()
+    unique_ips = set()
+
+    commands_entered = dict()
+    passwords_attempted = dict()
+    connections_by_country = dict()
     
     file_name = sys.argv[1];
     file = open(file_name)
-    for line in file.readlines():
+    
+    for line in tqdm(file.readlines()):
         
         line_object = json.loads(line)
+        
         eventID = line_object.get("eventid")
+        ip = line_object.get("src_ip")
+        username = line_object.get("username")
+        password = line_object.get("password")
+        
+        if (eventID == "cowrie.session.connect") & (not(ip in unique_ips)):
+            
+            unique_ips.add(ip)
+            details = handler.getDetails(ip)
+            country = details.country_name
+            
+            if (country in connections_by_country):
+                connections_by_country[country] = connections_by_country[country] + 1
+            else:
+                connections_by_country[country] = 1
+            continue
         
         if eventID == "cowrie.login.failed":
             countFailed+=1
-            ip = line_object.get("src_ip")
-            username = line_object.get("username")
-            password = line_object.get("password")
-            
-            '''
-            #carefull with these calls, only have limited runs
-            print(ip, username, password)
-            details = handler.getDetails(ip)
-            print(details.country_name)
-            '''
             
             if username in usernames_failedlogin:
                 usernames_failedlogin[username] = usernames_failedlogin[username] + 1
@@ -58,13 +68,11 @@ def main():
                 passwords_attempted[password] = passwords_attempted[password] + 1
             else:
                 passwords_attempted[password] = 1
+            continue
             
             
         if eventID == "cowrie.login.success":
             countSuccessful+=1
-            username = line_object.get("username")
-            password = line_object.get("password")
-            ip = line_object.get("src_ip")
             
             if username in usernames_succlogin:
                 usernames_succlogin[username] = usernames_succlogin[username] + 1
@@ -83,26 +91,31 @@ def main():
                     count = ip_failedlogin[ip]
                     if count > max_root_attempts:
                         max_root_attempts = count
-
+            continue
+        
         if eventID == "cowrie.command.input":
             cmd = line_object.get("input")
             if cmd in commands_entered:
                 commands_entered[cmd] = commands_entered[cmd] + 1
             else:
                 commands_entered[cmd] = 1
+            continue
     
     file.close()
     
     print("Failed attempts:\n\t", countFailed)
     print("Successful attempts:\n\t", countSuccessful)
-    print("Most common username for failed attempts:\n\t",max(usernames_failedlogin, key=lambda k: usernames_failedlogin[k]))
-    print("Most common username for successful attempts:\n\t",max(usernames_succlogin, key=lambda k: usernames_succlogin[k]))
+    print("Most common username for failed attempts:\n\t",\
+            max(usernames_failedlogin, key=lambda k: usernames_failedlogin[k]))
+    print("Most common username for successful attempts:\n\t",\
+            max(usernames_succlogin, key=lambda k: usernames_succlogin[k]))
     print("Top ten passwords for all attempts (descending):")
     for i in range(10):
         toPop = max(passwords_attempted, key=lambda k: passwords_attempted[k])
         print("\t",toPop)
         passwords_attempted.pop(toPop)
-    print("Source IP with most unsuccessful logins:\n\t",max(ip_failedlogin, key=lambda k: ip_failedlogin[k]))
+    print("Source IP with most unsuccessful logins:\n\t",\
+            max(ip_failedlogin, key=lambda k: ip_failedlogin[k]))
     print("Unique addresses with root login on first attempt:\n\t", ip_firsttime.__len__())
     print("Max amount of failed root logins before a successful login (assuming a single IP):\n\t",max_root_attempts)
     print("Top five most performed commands post-login:")
@@ -111,6 +124,13 @@ def main():
         print("\t", toPop)
         commands_entered.pop(toPop)
     print("See line 4697 for attempted public key write to authorized_keys")
+    
+    #'''
+    #This data will be included in report
+    print("Country, Count")
+    for key in connections_by_country:
+        print(key,",",connections_by_country[key])
+    #'''
 
 if __name__ == "__main__":
     main()
